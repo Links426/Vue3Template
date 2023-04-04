aa1f0a2d79d70bf5a2c911dfbf3ce6388dbba3c9
XLin2048 <1982864305@qq.com>
2023-04-04 20:03:31 +0800

    feat: 🎸 完成模版搭建

--- src/router/router.ts
+++ src/router/router.ts
@@ -4,62 +4,7 @@ const routes = [
         component: () => import('@/views/index/index.vue'),
         redirect: '/main',
         meta: { label: '主页' },
-        children: [
-            {
-                path: '/main',
-                name: '工作台',
-                component: () => import('@/views/main/main.vue'),
-                meta: { label: '工作台', openKeys: ['0'], selectKeys: ['0_0'] },
-            },
-            {
-                path: '/data',
-                name: '数据可视化',
-                redirect: '/data/analyse',
-                meta: { label: '数据可视化' },
-                children: [
-                    {
-                        path: 'analyse',
-                        name: '分析页',
-                        component: () => import('@/views/data/analyse.vue'),
-                        meta: {
-                            label: '分析页',
-                            openKeys: ['1'],
-                            selectKeys: ['1_0'],
-                        },
-                    },
-                ],
-            },
-            {
-                path: '/personal',
-                name: '个人中心',
-                redirect: '/personal/info',
-                meta: {
-                    label: '个人中心',
-                },
-                children: [
-                    {
-                        path: 'info',
-                        name: '个人信息',
-                        component: () => import('@/views/personal/info.vue'),
-                        meta: {
-                            label: '个人信息',
-                            openKeys: ['2'],
-                            selectKeys: ['2_0'],
-                        },
-                    },
-                    {
-                        path: 'setting',
-                        name: '个人设置',
-                        component: () => import('@/views/personal/setting.vue'),
-                        meta: {
-                            label: '个人设置',
-                            openKeys: ['2'],
-                            selectKeys: ['2_1'],
-                        },
-                    },
-                ],
-            },
-        ],
+        children: [],
     },
 ]
 export default routes
